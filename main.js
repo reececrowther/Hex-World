9b7b9f21cf6ff5002e5d035891ae6f8fc2ee8046
Reece Crowther <reececrowther@hotmail.co.uk>
2021-12-14 19:38:32 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import '/style.css'
+import '//reececrowther.github.io/Hex-World/style.css'
 
-import * as THREE from '//cdn.skypack.dev/three/build/three';
+import * as THREE from '//cdn.skypack.dev/three/build/three.module.js';
 import randomFloat from '//reececrowther.github.io/Hex-World/random-float';
 import { GLTFLoader } from '//cdn.skypack.dev/three/examples/jsm/loaders/GLTFLoader.js';
 import { EffectComposer } from '//cdn.skypack.dev/three/examples/jsm/postprocessing/EffectComposer.js';
